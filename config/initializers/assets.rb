2abf9dc7c6549fc8ca224c4e550059a650a952b8
Justin Coyne <jcoyne@justincoyne.com>
2015-09-09 23:36:00 -0500

    Configure babel to make AMD requires

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,12 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( manifest.js application.css application.js )
+
+Rails.application.config.assets.configure do |env|
+  es6amd = Sprockets::BabelProcessor.new('modules' => 'amd', 'moduleIds' => true)
+  # Replace the default transformer to transpile each `.es6` file with `define`
+  # and `require` from the AMD spec.
+  # Just be sure to add `almond.js` to the application and
+  # require it before requiring other assets on `application.js`
+  env.register_transformer 'application/ecmascript-6', 'application/javascript', es6amd
+end
